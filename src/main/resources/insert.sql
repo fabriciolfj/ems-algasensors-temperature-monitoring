040b14823e10a70213e79ef70f864b240bd82cea
spark <fabricio.jacob@outlook.com>
2025-06-17 22:20:21 -0300

    init project.

---
+++ src/main/resources/insert.sql
@@ -0,0 +1,3 @@
+-- Insert 1: Sensor com temperatura normal e habilitado
+INSERT INTO sensor_monitoring (id, last_temperature, updated_at, enabled)
+VALUES ("0K6ZYEYMTZ8QT", 23.5, '2025-06-17T10:30:00-03:00', true);
